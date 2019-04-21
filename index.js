c1c1d0a2f03a954b68e4504c3b569c0208c9cf68
myishc <844271143@qq.com>
2019-04-21 20:56:21 +0800

    完成判断用户是否登录 2019/4/21 21:00

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//导入模块
+/*------------------------导入模块--------------------------- */
 const mongodb = require('./tool/02.mongodb抽取'),
     express = require('express'),
     multer = require('multer'),
@@ -8,14 +8,41 @@ const mongodb = require('./tool/02.mongodb抽取'),
         dest: './views/imgs'
     }),
     svgCaptcha = require('svg-captcha'),
- 
 
-    //实例化服务器
+    cookieSession = require('cookie-session'),
+
+
+/*------------------------实例化服务器--------------------------- */
     app = express()
 
+//注册中间件
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+//注册中间件
+app.use(cookieSession({
+    name: 'session',
+    keys: ['captcha', 'userName'],
+    maxAge: 24 * 60 * 60 * 1000
+  }))
+
+  //判断用户是否登录
+app.use((req,res,next)=>{
+    if(req.url.indexOf('/hero')==0){
+        if(req.session.userName){
+            next()
+        }else{
+            res.send({
+                mes: '请先登录哦',
+                code: 403
+            })
+        }
+        
+    }else{
+        next()
+    }
+})
+
 //托管静态文件
 app.use(express.static('./views'))
 
@@ -64,7 +91,7 @@ app.get('/heroDetail', (req, res) => {
 })
 
 //新增英雄逻辑
-app.post('/addHero', upload.single('heroIcon'), (req, res) => {
+app.post('/heroAdd', upload.single('heroIcon'), (req, res) => {
     const heroName = req.body.heroName,
         skillName = req.body.skillName,
         heroIcon = path.join('imgs', req.file.filename)
@@ -83,7 +110,7 @@ app.post('/addHero', upload.single('heroIcon'), (req, res) => {
 })
 
 //修改英雄逻辑
-app.post('/updateHero', upload.single('heroIcon'), (req, res) => {
+app.post('/heroUpdate', upload.single('heroIcon'), (req, res) => {
     const heroName = req.body.heroName,
         skillName = req.body.skillName,
         id = req.body.id
@@ -120,7 +147,7 @@ app.post('/updateHero', upload.single('heroIcon'), (req, res) => {
 })
 
 //删除英雄逻辑
-app.get('/deleteHero', (req, res) => {
+app.get('/heroDelete', (req, res) => {
     const id = req.query.id
 
     mongodb.deleteOne('cqlist', {
@@ -159,16 +186,61 @@ app.post('/userRegister', (req, res) => {
 //验证码
 app.get('/captcha', function (req, res) {
     var captcha = svgCaptcha.create();
-    // req.session.captcha = captcha.text;
+    req.session.captcha = captcha.text;
     console.log(captcha.text);
     res.type('svg');
     res.status(200).send(captcha.data);
 });
 
 // 用户登录逻辑
-// app.post('/userLogin',(req,res)=>{
-//     mongodb.find
-// })
+app.post('/userLogin',(req,res)=>{
+    const captcha = req.body.captcha,
+        userName = req.body.userName,
+         password = req.body.password
+    
+    if(req.session.captcha.toLowerCase()==captcha.toLowerCase()){
+        console.log(userName,password);
+        mongodb.find('userList',{
+            userName,
+            password
+        },result=>{
+            // res.send(result)
+            console.log(result);
+            if(result.length!=0){
+                req.session.userName = userName
+                res.send({
+                    mes: '登陆成功',
+                    code: 1,
+                    userName
+                })
+            }else{
+                res.send({
+                    mes: '账号密码有误',
+                    code: 0
+                })
+            }
+        })
+    }else{
+        res.send({
+            mes: '验证码有误',
+            code: 0
+        })
+    }
+})
+
+// 登出接口
+app.get('/heroUserLogout',(req,res)=>{
+    req.session = null
+    res.send({
+        mes: '退出成功',
+        code: 1
+    })
+})
+
+//判断是否登录接口
+app.get('/heroUser',(req,res)=>{
+    res.send()
+})
 
 //开启监听
 app.listen(600)
